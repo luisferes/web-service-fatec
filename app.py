6668939e116a869a549fbb8764fa43c44d47399f
Luís Felipe <luisfelipe.fsantos@gmail.com>
2025-10-31 20:03:50 -0300

    primeira versao da aplicacao

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-# WEB SERVICE FATEC
+# 1. Importe a classe Flask
+from flask import Flask
+
+# 2. Crie uma instância da classe Flask
+# __name__ é uma variável especial do Python
+app = Flask(__name__)
+
+# 3. Defina uma rota (caminho) e a função que ela chamará
+# O decorador @app.route('/') liga a URL raiz a função home()
+@app.route('/')
+def home():
+  return 'Olá, Mundo Real!'
+
+@app.route('/usuarios')
+def usuarios():
+  return 'Olá, Usuários!'
+
+# 4. Execute o aplicativo
+if __name__ == '__main__':
+  # app.run() inicia o servidor web de desenvolvimento
+  # debug=True permite que o servidor reinicie automaticamente ao salvar o arquivo
+  app.run(debug=True)
